9013ced72312606780726a3060a7679e825d1a67
Kim Capilla <kcapilla@Kims-MBP.attlocal.net>
2021-12-03 10:05:39 -0500

    blah

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import {BASE_URL, config} from "./services/index"
 
 function App() {
+  const [items, setItems] = useState('')
+
+  useEffect(() => {
+    const getAllItems = async () => {
+      const response = await axios.get(BASE_URL, config)
+      setItems(response.data.records)
+    }
+    getAllItems()
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>test</h1>
     </div>
   );
 }
---
+++ client/src/services/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export const BASE_URL =
+  `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/items`
+
+export const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+  }
+}
